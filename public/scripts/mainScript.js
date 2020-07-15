dc656eb7e1552716fa15dffa86babc6b1cb03277
Joshua Patterson <joshkpatt@gmail.com>
2020-07-15 17:48:36 -0600

    Initial commit

---
+++ public/scripts/mainScript.js
@@ -0,0 +1,83 @@
+
+let menuState = 1; // to determine whether to delay class toggle; 1 means box is expanded, 0 is collapsed. delay on 0
+let ddaState = 0;
+let dd6State = 0;
+let dd5State = 0;
+
+
+let map, marker;
+const mapDiv = document.querySelector('#map');
+const modal = document.querySelector('#modal');
+
+function initMap(){ 
+  map = new google.maps.Map(mapDiv, {zoom: 18, mapTypeId: 'satellite', disableDefaultUI: true, center: {lat: 39.5210, lng: -76.6461}})
+}
+function moveMap(lat, lng, zoom){
+  map = new google.maps.Map(mapDiv, {zoom: zoom, mapTypeId: 'satellite', disableDefaultUI: true, center: {lat: lat, lng: lng}})
+  // hamburger();
+}
+
+function openModal(){
+  modal.style.display = 'block';
+}
+
+function closeModal(){
+  modal.style.display = 'none';
+}
+
+
+function hamburger(){
+  const menu = document.querySelector('#menu');
+  menu.classList.toggle('collapsed');
+  menu.classList.toggle('expanded');
+
+  const menuContent = document.querySelector('#menuContent');
+  let swap = async function(){
+    menuContent.classList.toggle('collapsed')
+    menuContent.classList.toggle('expanded')
+  }
+  if(menuState){ //Checking to see if menu is open
+    menuState = 0;
+    swap();
+  } else {
+    menuState = 1;
+    setTimeout(swap, 200);
+  }
+  document.querySelector('#box').classList.toggle('open');
+}
+
+function drop1(){
+  if(ddaState){
+    document.querySelector('#allWondersDD').classList.toggle('showDisplay');
+    setTimeout(function(){document.querySelector('#allWondersDD').classList.toggle('showDisplayOpacity')}, 500)
+    ddaState = 0;
+  } else {
+    document.querySelector('#allWondersDD').classList.toggle('showDisplay');
+    document.querySelector('#allWondersDD').classList.toggle('showDisplayOpacity');
+    ddaState = 1;
+  }
+}
+
+function drop2(){
+  if(dd6State){
+    document.querySelector('#c6WondersDD').classList.toggle('showDisplay');
+    setTimeout(function(){document.querySelector('#c6WondersDD').classList.toggle('showDisplayOpacity')}, 500)
+    dd6State = 0;
+  } else {
+    document.querySelector('#c6WondersDD').classList.toggle('showDisplay');
+    document.querySelector('#c6WondersDD').classList.toggle('showDisplayOpacity');
+    dd6State = 1;
+  }
+}
+
+function drop3(){
+  if(dd5State){
+    document.querySelector('#c5WondersDD').classList.toggle('showDisplay');
+    setTimeout(function(){document.querySelector('#c5WondersDD').classList.toggle('showDisplayOpacity')}, 500)
+    dd5State = 0;
+  } else {
+    document.querySelector('#c5WondersDD').classList.toggle('showDisplay');
+    document.querySelector('#c5WondersDD').classList.toggle('showDisplayOpacity');
+    dd5State = 1;
+  }
+}
---
+++ server.js
@@ -0,0 +1,61 @@
+const express = require('express');
+const fs = require('fs');
+const nodeMailer = require('nodemailer');
+const bodyParser = require('body-parser');
+const app = express();
+require('dotenv').config()
+// console.log(process.env.MAPS_API_KEY)
+
+app.set('view engine', 'ejs');
+app.use(bodyParser.json()); 
+app.use(bodyParser.urlencoded({ extended: true })); 
+app.use(express.static(__dirname + "/public"));
+const allwonders = JSON.parse(fs.readFileSync('./public/data/allwonders.JSON', 'utf-8'));
+const civ6wonders = JSON.parse(fs.readFileSync('./public/data/civ6wonders.JSON', 'utf-8'));
+const civ5wonders = JSON.parse(fs.readFileSync('./public/data/civ5wonders.JSON', 'utf-8'));
+// console.log(civ6wonders);
+// console.log(civ5wonders);
+
+app.get('/', (req, res) => {
+  res.render('main', {
+    allwonders: allwonders, 
+    civ6wonders: civ6wonders, 
+    civ5wonders: civ5wonders, 
+    apiKey: process.env.MAPS_API_KEY
+  });
+})
+
+app.get('/feedback', (req, res) => {
+  res.render('feedback');
+})
+
+app.post('/', (req, res) => {
+  res.render('submission');
+  let feedback = req.body.feedback;
+  console.log(feedback);
+  let transporter = nodeMailer.createTransport({
+    service: 'gmail',
+    auth: {
+      user: process.env.EMAIL_USER,
+      pass: process.env.EMAIL_PASS
+    },
+    tls: {
+      rejectUnauthorized: false
+    }
+  });
+  let mailOptions = {
+    from: process.env.EMAIL_USER,
+    to: process.env.EMAIL_USER,
+    subject: `CivPlaces Feedback ${Date()}`,
+    text: feedback
+  }
+  transporter.sendMail(mailOptions, (err, info) => {
+    if (err) {
+      console.log(err)
+    } else {
+      console.log(`Email sent: ${info.response}`)
+    }
+  })
+})
+
+app.listen(3000);
